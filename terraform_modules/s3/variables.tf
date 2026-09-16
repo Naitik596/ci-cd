fe7a55fb40b68021e4759030e58e9db57ab0a869
Naitik596 <57811289+Naitik596@users.noreply.github.com>
2026-09-16 12:45:41 +0530

    Commit s3 module variables.tf

--- terraform_modules/s3/variables.tf
+++ terraform_modules/s3/variables.tf
@@ -1,54 +1,80 @@
 variable "name" {
-  description = "Name tag for all resources"
-  type        = string
-}
+	  description = "Name tag for all resources"
+	  type        = string
+	}
 
-variable "create_s3" {
-  description = "Master toggle to create S3 resources"
-  type        = bool
-  default     = true
-}
+	variable "create_s3_bucket" {
+	  description = "Master toggle for S3 bucket creation"
+	  type        = bool
+	  default     = true
+	}
 
-variable "bucket" {
-  description = "Name of the bucket. Must be lowercase and follow S3 naming rules."
-  type        = string
-  default     = null
-}
 
-variable "force_destroy" {
-  description = "Boolean that indicates all objects should be deleted from the bucket when destroyed."
-  type        = bool
-  default     = false
-}
 
-variable "versioning_configuration" {
-  description = "Configuration block for S3 bucket versioning."
-  type        = any
-  default     = { status = "Enabled" }
-}
+	variable "bucket" {
+	  description = "Name of the bucket. If omitted, Terraform will assign a random, unique name"
+	  type        = string
+	  default     = null
+	}
 
-variable "server_side_encryption_configuration" {
-  description = "Configuration for server-side encryption using KMS CMK."
-  type        = any
-  default = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = ""
-      }
-      bucket_key_enabled = true
-    }
-  }
-}
+	variable "versioning" {
+	  description = "Versioning configuration block"
+	  type = object({
+	    enabled = bool
+	  })
+	  default = {
+	    enabled = true
+	  }
+	}
 
-variable "attach_deny_insecure_transport_policy" {
-  description = "Controls if S3 bucket should have deny non-SSL transport policy attached."
-  type        = bool
-  default     = true
-}
+	variable "server_side_encryption_configuration" {
+	  description = "Server-side encryption configuration for the bucket"
+	  type = object({
+	    rule = object({
+	      apply_server_side_encryption_by_default = object({
+	        sse_algorithm = string
+	      })
+	    })
+	  })
+	  default = {
+	    rule = {
+	      apply_server_side_encryption_by_default = {
+	        sse_algorithm = "aws:kms"
+	      }
+	    }
+	  }
+	}
 
-variable "tags" {
-  description = "Tags applied to all resources"
-  type        = map(string)
-  default     = {}
-}
+	variable "block_public_access" {
+	  description = "Public access block configuration"
+	  type = object({
+	    block_public_acls       = bool
+	    block_public_policy     = bool
+	    ignore_public_acls      = bool
+	    restrict_public_buckets = bool
+	  })
+	  default = {
+	    block_public_acls       = true
+	    block_public_policy     = true
+	    ignore_public_acls      = true
+	    restrict_public_buckets = true
+	  }
+	}
+
+	variable "region" {
+	  description = "Region where the resource(s) will be managed"
+	  type        = string
+	  default     = "us-east-1"
+	}
+
+	variable "tags" {
+	  description = "Tags applied to all resources"
+	  type        = map(string)
+	  default     = {}
+	}
+
+	variable "s3_bucket_tags" {
+	  description = "Additional tags for the S3 bucket"
+	  type        = map(string)
+	  default     = {}
+	}
